41d7c88f799eedab60dc4d87df6245b3265fe817
Ferran <fefu695@vidalibarraquer.net>
2023-05-12 10:37:29 +0200

    Push test sumaValorAbsolut

--- src/main/java/com/mycompany/suma/Suma.java
+++ src/main/java/com/mycompany/suma/Suma.java
@@ -2,7 +2,8 @@ package com.mycompany.suma;
 
 public class Suma {
 
-    public double getNum1(double numeroA) {
-        return numeroA;
+    public double sumaValorAbsolut(double numeroA, double numeroB) {
+        double suma = Math.abs(numeroA) + Math.abs(numeroB);
+        return suma;
     }
 }
