8677d981a38de48431e733f06518d1dfa2fc3b5f
Ferran <fefu695@vidalibarraquer.net>
2023-05-12 10:30:45 +0200

    Push test getNum1

--- src/main/java/com/mycompany/suma/Suma.java
+++ src/main/java/com/mycompany/suma/Suma.java
@@ -2,9 +2,7 @@ package com.mycompany.suma;
 
 public class Suma {
 
-    public double Sumar(double numeroA, double numeroB) {
-        double suma = numeroA + numeroB;
-        return suma;
+    public double getNum1(double numeroA) {
+        return numeroA;
     }
-
 }
